ccb53b86074f17b36d30d6b80c9c71b36e76d05b
Sue <sue.park@theplatform.com>
2017-04-29 15:48:48 -0400

    fix 5

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Collective/Remote/RemoteServiceProvider::class,
+        Collective\Remote\RemoteServiceProvider::class,
     ],
 
     /*
