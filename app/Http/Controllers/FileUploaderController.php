49ec03bbbb016068422ef021cd7df60e6602b8ba
Sue <sue.park@theplatform.com>
2017-04-29 15:47:45 -0400

    add SSH

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use SSH;
 
 class FileUploaderController extends BaseController
 {
 
     public function deploy()
     {
-        /*SSH::into('production')->run(array(
+        SSH::into('production')->run(array(
             'cd ~/public_html/mywebsite',
             'git pull origin master'
         ), function($line){
         
             echo $line.PHP_EOL; // outputs server feedback
-        });*/
+        });
     }
 }
