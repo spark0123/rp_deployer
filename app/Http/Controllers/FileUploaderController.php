ae1f9a400559185c8451ed9d9ae6857f610fbae5
Sue <sue.park@theplatform.com>
2017-04-30 19:20:27 -0400

    upload file individually

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -30,9 +30,42 @@ class FileUploaderController extends Controller
           return response()->json(['status' => 'fail', 'message' => 'unzip failed.']);
         }
 
-        SSH::into('production')->put('/tmp/rp_common_vod/rp_common_vod-master/*', '/448004/sue_test/master/*');
+        $local_directory = "/tmp/rp_common_vod/rp_common_vod-master/";
+        $remote_directory = "/448004/sue_test/"
+        /* We save all the filenames in the following array */
+        $files_to_upload = array();
+         
+        /* Open the local directory form where you want to upload the files */
+        if ($handle = opendir($local_directory)) 
+        {
+            /* This is the correct way to loop over the directory. */
+            while (false !== ($file = readdir($handle))) 
+            {
+                if ($file != "." && $file != "..") 
+                {
+                    $files_to_upload[] = $file;
+                }
+            }
+         
+            closedir($handle);
+        }
+         
+        if(!empty($files_to_upload))
+        {
+            /* Now upload all the files to the remote server */
+            foreach($files_to_upload as $file)
+            {
+                  /* Upload the local file to the remote server */
 
-        return response()->json(['status' => 'success']);
+                  $success = SSH::into('production')->put($local_directory . $file, $remote_directory . $file);
+            }
+        }
+        
+
+        if($success)
+            return response()->json(['status' => 'success']);
+        else
+            return response()->json(['status' => 'fail']);
 
         //$this->deleteDirectory('/tmp/rp_common_vod');
     }
