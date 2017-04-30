884c49bf348eea2da6f8ab0d3d7cd5c9ba10a015
Sue <sue.park@theplatform.com>
2017-04-30 12:54:52 -0400

    test date

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -10,12 +10,14 @@ class FileUploaderController extends Controller
         /*
 sftp -oIdentityFile=/Users/sue.park/Downloads/rationalized_key.rsa -oHostKeyAlgorithms=+ssh-dss sshacs@tverationalstg.upload.akamai.com
         */
-        SSH::into('production')->run(array(
+		
+        /*SSH::into('production')->run(array(
             'cd ~/public_html/mywebsite',
             'git pull origin master'
         ), function($line){
         
             echo $line.PHP_EOL; // outputs server feedback
-        });
+        });*/
+        SSH::into('production')->run('date', function($line) {dd($line); });
     }
 }
