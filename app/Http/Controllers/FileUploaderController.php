378fb73208a9e578258709971a8079c0b1383157
Sue <sue.park@theplatform.com>
2017-04-30 16:09:54 -0400

    update 11

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -9,60 +9,59 @@ class FileUploaderController extends Controller
 
     public function deploy()
     {
-    	//get master from github
-    	//unzip in local
-    	//deploy to sftp
-    	//delete local files
-    	if (!is_dir('/tmp/rp_common_vod')) {
-		    mkdir('/tmp/rp_common_vod');
-		}
+        //get master from github
+        //unzip in local
+        //deploy to sftp
+        //delete local files
+        if (!is_dir('/tmp/rp_common_vod')) {
+            mkdir('/tmp/rp_common_vod');
+        }
 
 
         file_put_contents("/tmp/rp_common_vod/master.zip", 
-		    file_get_contents("https://github.com/spark0123/rp_common_vod/archive/master.zip")
-		);
+            file_get_contents("https://github.com/spark0123/rp_common_vod/archive/master.zip")
+        );
 
-		$zip = new ZipArchive;
-		$res = $zip->open('/tmp/rp_common_vod/master.zip');
-		if ($res === TRUE) {
-		  $zip->extractTo('/tmp/rp_common_vod');
-		  $zip->close();
-		} else {
-		  echo 'unzip failed';
-		}
+        $zip = new ZipArchive;
+        $res = $zip->open('/tmp/rp_common_vod/master.zip');
+        if ($res === TRUE) {
+          $zip->extractTo('/tmp/rp_common_vod');
+          $zip->close();
+        } else {
+          echo 'unzip failed';
+        }
 
-		// upload files to remote
-		SSH::into('production')->run(array(
+        // upload files to remote
+        /*SSH::into('production')->run(array(
             'put -r /tmp/rp_common_vod/rp_common_vod-master/* sue_test'
         ), function($line)
-		{
-			echo $line.PHP_EOL;
-		});
+        {
+            echo $line.PHP_EOL;
+        });
 
-		$this->deleteDirectory('/tmp/rp_common_vod');
+        $this->deleteDirectory('/tmp/rp_common_vod');*/
 
-        //SSH::into('production')->run('date', function($line) { echo $line; });
     }
     private function deleteDirectory($dir) {
-	    if (!file_exists($dir)) {
-	        return true;
-	    }
+        if (!file_exists($dir)) {
+            return true;
+        }
 
-	    if (!is_dir($dir)) {
-	        return unlink($dir);
-	    }
+        if (!is_dir($dir)) {
+            return unlink($dir);
+        }
 
-	    foreach (scandir($dir) as $item) {
-	        if ($item == '.' || $item == '..') {
-	            continue;
-	        }
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
 
-	        if (!$this->deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
-	            return false;
-	        }
+            if (!$this->deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+                return false;
+            }
 
-	    }
+        }
 
-	    return rmdir($dir);
-	}
+        return rmdir($dir);
+    }
 }
