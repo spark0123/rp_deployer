7a37321763d01e231b8de9c81b76780674813952
Sue <sue.park@theplatform.com>
2017-04-30 17:28:24 -0400

    test

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -38,7 +38,7 @@ class FileUploaderController extends Controller
             return response()->json(['status' => 'success', 'message' => $line.PHP_EOL]);
         });*/
 
-        $ssh_msg = SSH::into('production')->exists( '/448004/sue_test/test2.txt.js');
+        $ssh_msg = SSH::into('production')->exists( '/448004/sue_test/test2.txt');
         return response()->json(['status' => 'success', 'message' => $ssh_msg]);
 
         //SSH::into('production')->put('/tmp/rp_common_vod/rp_common_vod-master/css/rational-cc-panel.css', 'sue_test');
