f84be3836fd9de104119de0f1844ff43a69ec962
Sue <sue.park@theplatform.com>
2017-04-30 19:53:26 -0400

    a

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -33,10 +33,10 @@ class FileUploaderController extends Controller
         $local_directory = "/tmp/rp_common_vod/rp_common_vod-master/";
         $remote_directory = "/448004/sue_test/";
         
-        $success = $this->dirToArray($local_directory); //$this->uploadAll($local_directory,$remote_directory);
+        $success = $this->uploadAll($local_directory,$remote_directory);
 
         if($success)
-            return response()->json(['status' => 'success','message' => $success]);
+            return response()->json(['status' => 'success']);
         else
             return response()->json(['status' => 'fail']);
 
@@ -68,36 +68,17 @@ class FileUploaderController extends Controller
 
     private function uploadAll($local_directory, $remote_directory){
         /* We save all the filenames in the following array */
-        $files_to_upload = array();
-         
-        /* Open the local directory form where you want to upload the files */
-        if ($handle = opendir($local_directory)) 
-        {
-            /* This is the correct way to loop over the directory. */
-            while (false !== ($file = readdir($handle))) 
-            {
-                if(is_dir($file)){
-                    $this->uploadAll($local_directory.$file.'/',$remote_directory.$file.'/');
-                    break;
-                }else{
-                    if ($file != "." && $file != "..") 
-                    {
-                        $files_to_upload[] = $file;
-                    }
-                }
-            }
-         
-            closedir($handle);
-        }
+        $files_to_upload = $this->dirToArray($local_directory);
          
         if(!empty($files_to_upload))
         {
             /* Now upload all the files to the remote server */
-            foreach($files_to_upload as $file)
+            foreach($files_to_upload as $key => $file)
             {
                   /* Upload the local file to the remote server */
-
-                  $success = SSH::into('production')->put($local_directory . $file, $remote_directory . $file);
+                  if($key != "0"){
+                      $success = SSH::into('production')->put($local_directory .'/' . $key .'/' . $file, $remote_directory .'/' . $key .'/' . $file);
+                  }
             }
         }
         return $success;
