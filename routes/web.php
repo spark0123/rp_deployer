df54ec366c19b3a7925b864e4d6996362bcd4e52
EC2 Default User <ec2-user@ip-172-31-7-161.ec2.internal>
2017-04-30 04:39:39 +0000

    fix8

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('deploy', 'FileUploaderController@deploy')->name('deploy');
-
+Route::get('deploy', 'FileUploaderController@deploy');
