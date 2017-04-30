e6fcc89a042f3bcc9ea884775be4979e43a6005d
Sue <sue.park@theplatform.com>
2017-04-30 13:06:02 -0400

    test

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -18,6 +18,12 @@ sftp -oIdentityFile=/Users/sue.park/Downloads/rationalized_key.rsa -oHostKeyAlgo
         
             echo $line.PHP_EOL; // outputs server feedback
         });*/
-        SSH::into('production')->run('date', function($line) { echo $line; });
+        // see if file exists on remote
+		SSH::into('production')->exists( '/448004/sue_test/share_config.js' );
+
+		// upload file to remote
+		SSH::into('production')->put( '/var/www/storage/logs/laravel.log', '/448004/sue_test/laravel.log' );
+
+        //SSH::into('production')->run('date', function($line) { echo $line; });
     }
 }
