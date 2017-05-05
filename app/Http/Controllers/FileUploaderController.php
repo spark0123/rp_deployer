c13b0d734c62a75e1f7c176630c1c3c95e581019
Sue <sue.park@theplatform.com>
2017-05-04 20:08:56 -0400

    test

--- app/Http/Controllers/FileUploaderController.php
+++ app/Http/Controllers/FileUploaderController.php
@@ -104,7 +104,7 @@ class FileUploaderController extends Controller
     https://api.github.com/repos/NBCU-PAVE/'.$repo_name.'/tarball/'.$tag.' | \
     tar xz --strip-components=1',$output);
         $local_directory = "/tmp/".$local_folder_name;
-        exec('scp -i /var/www/key/rationalized_key.rsa -rp '.$local_directory.' sshacs@tverationalstg.upload.akamai.com');
+        exec('scp -i /var/www/key/rationalized_key.rsa -rp '.$local_directory.' sshacs@tverationalstg.upload.akamai.com:sue_test');
        
         
         /*$dir_exist = SSH::into('production')->exists( $remote_directory  );
