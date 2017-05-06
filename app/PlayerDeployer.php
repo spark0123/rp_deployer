45755e4d72fe0e92fb8913ba5f9b2b9f90455e0f
Sue <sue.park@theplatform.com>
2017-05-06 19:11:08 -0400

    t

--- app/PlayerDeployer.php
+++ app/PlayerDeployer.php
@@ -7,7 +7,18 @@ use Illuminate\Notifications\Notifiable;
 class PlayerDeployer
 {
     use Notifiable;
-    private $slack_webhook_url = env('slack_webhook_url', '');
+    private $slack_webhook_url;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+        $this->slack_webhook_url = env('slack_webhook_url', '');
+    }
 
     /**
      * Route notifications for the Slack channel.
